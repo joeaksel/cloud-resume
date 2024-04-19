17027dbdff9d5604ee7e6fafb88d1c0a0f5a037a
Joe Aksel <joe.aksel@gmail.com>
2024-04-19 09:34:51 -0400

    New commit. Messed up my last repo, not sure what happened. Having to start over.

---
+++ frontend/main.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    getVisitCount();
+})
+
+const functionApiUrl = 'https://jakselcloudresume.azurewebsites.net/api/http_trigger?'
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApiUrl).then(response => {
+        return response.json()
+    }).then(response =>{
+        console.log("Website called function API.");
+        console.log(response.count)
+        count = response.count;
+        console.log(count)
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
+}
